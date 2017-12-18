e05ed31ecd5ede6628f0a239c261e1634af4a232
donnice <402696415@qq.com>
2017-12-17 19:59:24 -0800

    exit status code

---
+++ ch1/exit_status_alternative.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <pthread.h>
+
+pthread_t thread;
+static int arg;
+static const int internal_error = -12;
+static const int normal_error = -10;
+static const int success = 1;
+
+void *routine_x(void *arg_in)
+{
+	int *arg = (int *)arg_in;
+
+	if(*arg < 5) {
+		pthread_exit((void *)&internal_bad_error);
+	} else if(*arg < 10) {
+		return((void *)&normal_error);
+	} else {
+		return ((void *)&success);
+	}
+}
+
+extern int
+main(int argc, char **argv)
+{
+	pthread_t thread;
+	void *statusp;
+
+	pthread_create(&thread, NULL, routine_x, &arg);
+	pthread_join(thread, &statusp);
+	if(*statusp == PTHREAD_CANCELED) {
+		printf("Thread was cancelled.\n");
+	} else {
+		printf("Thread completed and exit status is %ld.\n", *(int *)statusp);
+	}
+	return 0;
+}
